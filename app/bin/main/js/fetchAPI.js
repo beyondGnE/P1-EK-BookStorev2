643990896ea6b182e55985d82225afe9f2c9d767
Eugene Kim <eugenehkim25@gmail.com>
2022-09-13 22:31:11 -0600

    Adding more features to frontend

---
+++ app/bin/main/js/fetchAPI.js
@@ -0,0 +1,9 @@
+const content = document.getElementById("content");
+
+document.getElementById("viewBooks").addEventListener("click", function() {
+    console.log("testing view books");
+});
+
+document.getElementById("addBook").addEventListener("click", function() {
+    console.log("testing add book");
+});
